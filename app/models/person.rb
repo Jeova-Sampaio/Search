4f28ef838643470d2da19fd0028503d485d4b592
Jeova <jeova_samp@hotmail.com>
2020-12-01 16:49:01 -0300

    ValidacaoCampoUnico

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ApplicationRecord
     # Validations
-    validates :name, presence: true
-    validates :phone, presence: true, length: { minimum: 11, maximum: 15 }, numericality: true
+    validates :name, presence: true, uniqueness: true
+    validates :phone, presence: true, length: { minimum: 11, maximum: 15 }, numericality: true, uniqueness: true
 end
